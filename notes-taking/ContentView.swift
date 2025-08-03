5ff6e5d7d4c0cf71dacb7eca3461817086aacf07
jonathanduran7 <jduran@genit.com.ar>
2025-08-02 22:11:25 -0300

    Refactor ContentView to use TabView for navigation, integrating HomeView, NotesView, CategoriesView, and SettingsView

--- notes-taking/ContentView.swift
+++ notes-taking/ContentView.swift
@@ -9,31 +9,37 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-
     var body: some View {
-        VStack{
-            TopBar(title: "Mis Notas")
-
-            SearchBar()
-
-            Spacer().frame(height: 10)
-
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 10) {
-                    ForEach(1...4, id: \.self) { card in
-                        Cards()
-                    }
+        TabView {
+            // Pestaña Home
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
                 }
-                .padding()
-            }
-
-            Spacer().frame(height: 30)
-
-            HistoryNotes().padding()
-
-            Spacer()
+            
+            // Pestaña Notas
+            NotesView()
+                .tabItem {
+                    Image(systemName: "note.text")
+                    Text("Notas")
+                }
+            
+            // Pestaña Categorías
+            CategoriesView()
+                .tabItem {
+                    Image(systemName: "folder.fill")
+                    Text("Categorías")
+                }
+            
+            // Pestaña Settings
+            SettingsView()
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("Ajustes")
+                }
         }
-
+        .accentColor(Color(red: 0.51, green: 0.60, blue: 0.57))
     }
 }
 
